3166201a5ed64bb78b9a6aff7927066a790f353f
Micke <mikael.porttila@gmail.com>
2019-05-24 21:35:05 +0200

    PSET-001: Initial commit

---
+++ src/components/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Hello from './Hello.svelte';
+</script>
+
+<section>
+	<h1>Parcel + Electron + Svelte Template</h1>
+	<Hello/>
+</section>
+
+<style>
+
+	@font-face {
+		font-family: "FiraCodeCdnFallback";
+		src: url(https://cdn.jsdelivr.net/gh/tonsky/FiraCode@1.206/distr/fira_code.css);
+	}
+
+	:global(body){
+		margin: 10px;
+		padding: 0;
+		background-color: #eee;
+		font-family: "Fira Code", monospace, FiraCodeCdnFallback !important;
+		font-feature-settings: "calt" 1;
+		font-variant-ligatures: contextual;
+		font-weight: 500;
+	}
+
+	h1{
+		text-align: center;
+	}
+</style>
---
+++ src/components/Hello.svelte
@@ -0,0 +1,14 @@
+<script>
+  let name = '';
+</script>
+
+<div>
+  <h1>Hello {name}!</h1>
+  <input bind:value={name}/>
+</div>
+
+<style>
+  h1 {
+    color: hotpink;
+  }
+</style>
